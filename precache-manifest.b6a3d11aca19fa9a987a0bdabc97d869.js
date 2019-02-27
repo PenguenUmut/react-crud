b2619d22dbcbb736cbf28d474495d992e5c7f822
umutisik <uuisik@gmail.com>
2019-02-27 23:27:50 +0300

    Updates

---
+++ precache-manifest.b6a3d11aca19fa9a987a0bdabc97d869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e49355552c3b4fed99c3",
+    "url": "/react-crud/static/js/runtime~main.e4935555.js"
+  },
+  {
+    "revision": "7ffcd1f3991b9b63e548",
+    "url": "/react-crud/static/js/main.7ffcd1f3.chunk.js"
+  },
+  {
+    "revision": "9e6f1df45d5f5cf29fe9",
+    "url": "/react-crud/static/js/2.9e6f1df4.chunk.js"
+  },
+  {
+    "revision": "9e6f1df45d5f5cf29fe9",
+    "url": "/react-crud/static/css/2.423fcfbc.chunk.css"
+  },
+  {
+    "revision": "4ab3d1beb074260dde5e6d27e59959d8",
+    "url": "/react-crud/index.html"
+  }
+];
